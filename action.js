3a7b5a11cf2f134a61cb7b8e4e6bfdda8a4e1ff8
Piekarski Jacek <jpiekarski@M1402.local>
2018-01-11 15:25:42 +0100

    zad_19_3

---
+++ action.js
@@ -0,0 +1,46 @@
+import uuid from uuid;
+
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'DELETE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(text) {
+    return {
+        type: EDIT_COMMENT,
+        id: 20,
+        text: 'wyedytowany tekst komentarza'
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: DELETE_COMMENT,
+        id: 20 //przykładowy id komentarza do usunięcia
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        text,
+        id: 20 //przykładowy id komentarza do oceniania
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        text,
+        id: 20 //przykładowy id komentarza do oceniania
+    }
+}
